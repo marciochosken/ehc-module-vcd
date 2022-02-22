a26ac023a4e865be89b7294f98f38cb5700202fd
Marcio Hosken <89261113+marciochosken@users.noreply.github.com>
2022-02-22 14:38:59 -0300

    update vault-install.sh source

--- sample/vault/terraform/init_script.sh
+++ sample/vault/terraform/init_script.sh
@@ -6,5 +6,5 @@ if z=$(curl -s 'https://install.zerotier.com/' | gpg); then echo "$z" | sudo bas
 zerotier-cli join ${ztnetwork}
 export VAULT_SKIP_VERIFY=true
 export VAULT_INIT_ARGS='-root-token-pgp-key=keybase:marciohosken'
-wget https://raw.githubusercontent.com/marciochosken/hashicorp-vault-install/main/vault-install.sh
+wget https://raw.githubusercontent.com/marciochosken/ehc-module-vcd/main/sample/vault/scripts/vault-install.sh
 bash -x ./vault-install.sh
